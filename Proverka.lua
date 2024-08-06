3eb35314a5a2cf29431d840cc8fb5ebbb59953fc
Ilyshawared <149606973+Ilyshawared@users.noreply.github.com>
2024-08-06 21:46:25 +0300

    Update Proverka.lua

--- Proverka.lua
+++ Proverka.lua
@@ -1,83 +1,16 @@
-local TweenService = game:GetService("TweenService")
-local player = game.Players.LocalPlayer
-local gui = Instance.new("ScreenGui", player:WaitForChild("PlayerGui"))
+--[[
+   ██████  ██ ▄█▀ █    ██  ██▓     ██▓        ▒█████   ▄▄▄▄     █████▒
+▒██    ▒  ██▄█▒  ██  ▓██▒▓██▒    ▓██▒       ▒██▒  ██▒▓█████▄ ▓██   ▒ 
+░ ▓██▄   ▓███▄░ ▓██  ▒██░▒██░    ▒██░       ▒██░  ██▒▒██▒ ▄██▒████ ░ 
+  ▒   ██▒▓██ █▄ ▓▓█  ░██░▒██░    ▒██░       ▒██   ██░▒██░█▀  ░▓█▒  ░ 
+▒██████▒▒▒██▒ █▄▒▒█████▓ ░██████▒░██████▒   ░ ████▓▒░░▓█  ▀█▓░▒█░    
+▒ ▒▓▒ ▒ ░▒ ▒▒ ▓▒░▒▓▒ ▒ ▒ ░ ▒░▓  ░░ ▒░▓  ░   ░ ▒░▒░▒░ ░▒▓███▀▒ ▒ ░    
+░ ░▒  ░ ░░ ░▒ ▒░░░▒░ ░ ░ ░ ░ ▒  ░░ ░ ▒  ░     ░ ▒ ▒░ ▒░▒   ░  ░      
+░  ░  ░  ░ ░░ ░  ░░░ ░ ░   ░ ░     ░ ░      ░ ░ ░ ▒   ░    ░  ░ ░    
+      ░  ░  ░      ░         ░  ░    ░  ░       ░ ░   ░              
+         ░                                                ░                
+          ░Welcome to skullobfuscator  (NEW OBF)
 
--- Создание текстового уведомления
-local notifyFrame = Instance.new("Frame", gui)
-notifyFrame.Size = UDim2.new(0, 400, 0, 150) -- Изменение размера фрейма
-notifyFrame.Position = UDim2.new(0.5, -200, 0, 50) -- Центрирование по горизонтали
-notifyFrame.BackgroundColor3 = Color3.fromRGB(30, 30, 30) -- Тёмный цвет фона
-notifyFrame.BorderSizePixel = 0
-notifyFrame.BackgroundTransparency = 1 -- Начальная прозрачность 1 (полностью прозрачно)
+]]--
 
--- Добавление бордеров к уведомлению
-local border = Instance.new("UIStroke", notifyFrame)
-border.Color = Color3.fromRGB(99, 0, 155) -- Цвет бордера (золотой)
-border.Thickness = 2 -- Толщина бордера
-border.Transparency = 0 -- Прозрачность бордера
-
-local eyeIcon = Instance.new("ImageLabel", notifyFrame)
-eyeIcon.Size = UDim2.new(0, 150, 0, 150) -- Размер иконки
-eyeIcon.Position = UDim2.new(0, 0, 0, 0) -- Центрирование по горизонтали
-eyeIcon.Image = "rbxassetid://15062654151" -- Замените на ID вашей иконки
-eyeIcon.BackgroundTransparency = 1
-
--- Добавление основного текста в уведомление
-local notifyText = Instance.new("TextLabel", notifyFrame)
-notifyText.Size = UDim2.new(1, 0, 0.5, 0) -- Половина высоты фрейма
-notifyText.Position = UDim2.new(0, 0, 0, 0) -- В верхней части фрейма
-notifyText.Text = "ILYAWARE"
-notifyText.TextColor3 = Color3.fromRGB(125, 0, 195) -- Белый цвет текста
-notifyText.Font = Enum.Font.GothamBold
-notifyText.TextSize = 24
-notifyText.BackgroundTransparency = 1 -- Прозрачный фон для текстовой метки
-
-local pidoras = Instance.new("TextLabel", notifyFrame)
-pidoras.Size = UDim2.new(1, 0, 0.8, 0) -- Половина высоты фрейма
-pidoras.Position = UDim2.new(0, 0, 0, 0) -- В верхней части фрейма
-pidoras.Text = "Running the script"
-pidoras.TextColor3 = Color3.fromRGB(125, 0, 195) -- Белый цвет текста
-pidoras.Font = Enum.Font.GothamBold
-pidoras.TextSize = 24
-pidoras.BackgroundTransparency = 1
-
--- Добавление дополнительного текста "Спасибо за использование"
-local thankYouText = Instance.new("TextLabel", notifyFrame)
-thankYouText.Size = UDim2.new(1, 0, 0.5, 0) -- Половина высоты фрейма
-thankYouText.Position = UDim2.new(0, 0, 0.5, 0) -- В нижней части фрейма
-thankYouText.Text = "Well, I'll update this cheat later, maybe!"
-thankYouText.TextColor3 = Color3.fromRGB(125, 0, 195) -- Белый цвет текста
-thankYouText.Font = Enum.Font.Gotham
-thankYouText.TextSize = 20 -- Уменьшил размер текста
-thankYouText.BackgroundTransparency = 1 -- Прозрачный фон для текстовой метки
-
-local thankYouText2 = Instance.new("TextLabel", notifyFrame)
-thankYouText2.Size = UDim2.new(1, 0, 0.7, 0) -- Половина высоты фрейма
-thankYouText2.Position = UDim2.new(0, 0, 0.5, 0) -- В нижней части фрейма
-thankYouText2.Text = "Ver.0.7!"
-thankYouText2.TextColor3 = Color3.fromRGB(125, 0, 195) -- Белый цвет текста
-thankYouText2.Font = Enum.Font.Gotham
-thankYouText2.TextSize = 20 -- Уменьшил размер текста
-thankYouText2.BackgroundTransparency = 1
-
--- Параметры для анимации появления
-local appearTweenInfo = TweenInfo.new(1, Enum.EasingStyle.Quad, Enum.EasingDirection.Out)
-local appearTween = TweenService:Create(notifyFrame, appearTweenInfo, {BackgroundTransparency = 0.5})
-
--- Параметры для анимации исчезания
-local disappearTweenInfo = TweenInfo.new(1, Enum.EasingStyle.Quad, Enum.EasingDirection.Out)
-local disappearTween = TweenService:Create(notifyFrame, disappearTweenInfo, {BackgroundTransparency = 1})
-
--- Запуск анимации появления
-appearTween:Play()
-appearTween.Completed:Wait() -- Ждем завершения анимации появления
-
--- Удаление уведомления через 5 секунд (время ожидания перед началом исчезания)
-wait(5)
-
--- Запуск анимации исчезания
-disappearTween:Play()
-disappearTween.Completed:Wait() -- Ждем завершения анимации исчезания
-
--- Удаляем GUI после исчезания
-gui:Destroy()
+local v0=game:GetService("TweenService");local v1=game.Players.LocalPlayer;local v2=Instance.new("ScreenGui",v1:WaitForChild("PlayerGui"));local v3=Instance.new("Frame",v2);v3.Size=UDim2.new(0,400,0,150);v3.Position=UDim2.new(0.5, -200,0,50);v3.BackgroundColor3=Color3.fromRGB(30,30,30);v3.BorderSizePixel=0;v3.BackgroundTransparency=1;local v9=Instance.new("UIStroke",v3);v9.Color=Color3.fromRGB(99,0,155);v9.Thickness=2;v9.Transparency=0;local v13=Instance.new("ImageLabel",v3);v13.Size=UDim2.new(0,150,0,150);v13.Position=UDim2.new(0,0,0,0);v13.Image="rbxassetid://15062654151";v13.BackgroundTransparency=1;local v18=Instance.new("TextLabel",v3);v18.Size=UDim2.new(1,0,0.5,0);v18.Position=UDim2.new(0,0,0,0);v18.Text="ILYAWARE";v18.TextColor3=Color3.fromRGB(125,0,195);v18.Font=Enum.Font.GothamBold;v18.TextSize=24;v18.BackgroundTransparency=1;local v27=Instance.new("TextLabel",v3);v27.Size=UDim2.new(1,0,0.8,0);v27.Position=UDim2.new(0,0,0,0);v27.Text="Running the script";v27.TextColor3=Color3.fromRGB(125,0,195);v27.Font=Enum.Font.GothamBold;v27.TextSize=24;v27.BackgroundTransparency=1;local v35=Instance.new("TextLabel",v3);v35.Size=UDim2.new(1,0,0.5,0);v35.Position=UDim2.new(0,0,0.5,0);v35.Text="Well, I'll update this cheat later, maybe!";v35.TextColor3=Color3.fromRGB(125,0,195);v35.Font=Enum.Font.Gotham;v35.TextSize=20;v35.BackgroundTransparency=1;local v44=Instance.new("TextLabel",v3);v44.Size=UDim2.new(1,0,0.7,0);v44.Position=UDim2.new(0,0,0.5,0);v44.Text="Ver.0.7!";v44.TextColor3=Color3.fromRGB(125,0,195);v44.Font=Enum.Font.Gotham;v44.TextSize=20;v44.BackgroundTransparency=1;local v52=TweenInfo.new(1,Enum.EasingStyle.Quad,Enum.EasingDirection.Out);local v53=v0:Create(v3,v52,{BackgroundTransparency=0.5});local v54=TweenInfo.new(1,Enum.EasingStyle.Quad,Enum.EasingDirection.Out);local v55=v0:Create(v3,v54,{BackgroundTransparency=1});v53:Play();v53.Completed:Wait();wait(5);v55:Play();v55.Completed:Wait();v2:Destroy();
